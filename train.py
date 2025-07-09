f3572a90dcc571c9e28cf7920eda12ad12d9fef3
Pakhi Chhetri <pakhichettri7@gmail.com>
2025-07-09 15:59:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -153,3 +153,17 @@ for fold, (train_idx, val_idx) in enumerate(kfold.split(X_all)):
             if trigger_times >= patience:
                 print(f"Early stopping at epoch {epoch+1}")
                 break
+
+model.load_state_dict(best_model_wts)
+    test_acc, test_f1, test_auc = evaluate(model, test_loader)
+    print(f"Fold {fold+1} Test: Accuracy={test_acc:.4f}, F1={test_f1:.4f}, AUROC={test_auc:.4f}")
+    all_test_metrics.append((test_acc, test_f1, test_auc))
+
+# Average Test Performance
+avg_acc = np.mean([m[0] for m in all_test_metrics])
+avg_f1 = np.mean([m[1] for m in all_test_metrics])
+avg_auc = np.mean([m[2] for m in all_test_metrics])
+
+print(f"\n Average Test Performance across 5 folds:")
+print(f"Accuracy: {avg_acc:.4f}, F1-Score: {avg_f1:.4f}, AUROC: {avg_auc:.4f}")
+
